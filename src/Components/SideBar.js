f8a6f76a11c44b936ffe0161f509bb3b3fd4e660
priyanka soni <ppriyankasoni811@gmail.com>
2021-12-29 14:39:13 +0530

    add navbar and sidebar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -7,88 +7,141 @@ export default function SideBar(props) {
         <ul className="items">
           <li className="bluebox">
             <i className="fa d fa-clock-o" aria-hidden="true"></i>
-            <Link to="/">dashboard</Link>
+            <Link style={{ color: "white", textDecoration: "none" }} to="/">
+              Dashboard
+            </Link>
           </li>
-
-          <br />
 
           <li>
             <i className="fas d fa-border-all"></i>
-            <Link to="/product">Product</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/product"
+            >
+              Product
+            </Link>
           </li>
-          <br />
 
           <li className="list-item">
             <i className="far d fa-heart"></i>
-            <Link to="/items">Items</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/items"
+            >
+              Items
+            </Link>
           </li>
-          <br />
 
           <li className="list-item">
             <i className="far d fa-comments-alt"></i>
-            <Link to="/Tabel">Tabel</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Tabel"
+            >
+              Tabel
+            </Link>
           </li>
-          <br />
 
           <li className="list-item">
             <i className="fas d fa-tasks"></i>
-            <Link to="/Favorite">Favorite</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Favorite"
+            >
+              Favorite
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="fas d fa-border-all"></i>
-            <Link to="/Inbox">Inbox</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Inbox"
+            >
+              Inbox
+            </Link>
           </li>
-          <br />
 
           <hr />
           <h5>pages</h5>
           <li className="list-item">
             <i className="fas d fa-gift"></i>
-            <Link to="/Pricing">Pricing</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Pricing"
+            >
+              Pricing
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="fas d fa-calendar-alt"></i>
-            <Link to="/calender">calender</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/calender"
+            >
+              calender
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="far d fa-clipboard-list"></i>
-            <Link to="/TO-Do">ToDo</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/TO-Do"
+            >
+              ToDo
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="far d fa-id-badge"></i>
-            <Link to="/contact">contact</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/contact"
+            >
+              contact
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="fas d fa-table"></i>
-            <Link to="/Invoice">Invoice</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Invoice"
+            >
+              Invoice
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="far d fa-signal-alt-3"></i>
-            <Link to="/UIelements">UIelements</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/UIelements"
+            >
+              UIelements
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="fas  d fa-user-plus"></i>
-            <Link to="/Team">Team</Link>
+            <Link style={{ color: "black", textDecoration: "none" }} to="/Team">
+              Team
+            </Link>
           </li>
-          <br />
           <li className="list-item">
             <i className="fas d fa-table"></i>
-            <Link to="/Tabel2">Tabel2</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Tabel2"
+            >
+              Tabel2
+            </Link>
           </li>
-          <br />
           <hr />
           <li className="list-item">
             <i className="fas d fa-cog"></i>
-            <Link to="/Setting">Setting</Link>
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Setting"
+            >
+              Setting
+            </Link>
           </li>
-          <br />
         </ul>
       </nav>
     </div>
